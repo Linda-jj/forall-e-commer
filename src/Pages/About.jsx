e2e3c3268e9d19018e42651457a942d24e1beac3
lideya <lideyafff@gmail.com>
2026-03-10 22:51:29 +0300

    finish the page

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Title from "../Componet/Title";
 import about from "../Assets/frontend_assets/about_img.png";
-
+import NewsletterBox from '../Componet/NewsletterBox'
 const About = () => {
   return (
     <div>
@@ -31,13 +31,35 @@ const About = () => {
           </div>
         </div>
         <div className="text-xl py-4 ">
-          <Title text1={'WHY '} text2={'CHOOSE US'}/>
+          <Title text1={"WHY "} text2={"CHOOSE US"} />
         </div>
         <div className="flex flex-col md:flex-row text-sm mb-20">
-<div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
-
-</div>
+          <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+            <b>Quality Assurance:</b>
+            <p className="text-gray-600">
+              We believe shopping should be more than just buying products — it
+              should be an experience that brings convenience, trust, and
+              happiness.
+            </p>
+          </div>
+          <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+            <b>Convenience:</b>
+            <p className="text-gray-600">
+              We believe shopping should be more than just buying products — it
+              should be an experience that brings convenience, trust, and
+              happiness.
+            </p>
+          </div>
+          <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+            <b>Exceptional Customer service:</b>
+            <p className="text-gray-600">
+              We believe shopping should be more than just buying products — it
+              should be an experience that brings convenience, trust, and
+              happiness.
+            </p>
+          </div>
         </div>
+        <NewsletterBox/>
       </div>
     </div>
   );
--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
-
+import React from "react";
+import Title from "../Componet/Title";
+import contact from "../Assets/frontend_assets/contact_img.png";
+import NewsletterBox from "../Componet/NewsletterBox";
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <div>
+      <div className="text-center text-2xl pt-10 border-t">
+        <Title text1={"CONTACT"} text2={"US"} />
+      </div>
+      <div className="my-10 flex flex-col justify-center md:flex-row gap-10 mb-28">
+        <img className="w-full md:max-w-[480px]" src={contact} alt="" />
+      <div className="flex flex-col justify-center items-start gap-6">
+<p className="font-semibold text-xl text-gray-600">Our Store</p>
+<p className="text-gray-500">57890 Bole subcity <b>Addis Ababa,Ethiopia</b></p>
+<p className="text-gray-500">tel:+251 923829008<br/>Email: admin@ForAll.com</p>
+<p className="font-semibold text-xl text-gray-600">Careers at ForAll </p>
+<p className="text-gray-500">Learn more about our teams and job openings.</p>
+<button className="border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500">Explore Jobs</button>
+      </div>
+      </div>
+      <NewsletterBox/>
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
